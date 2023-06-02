1d66421d7722e6656c8c84cacc74b3253d45dad3
Bhuwanxhettri <you@example.com>
2023-06-02 17:36:45 +0545

    first commit

---
+++ database/conn.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+mongoose
+  .connect("mongodb+srv://bhuwan:bhuwan123@carrental.bsteyhl.mongodb.net/?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("connection successful");
+  })
+  .catch((err) => {
+    console.log("no connection");
+  });
