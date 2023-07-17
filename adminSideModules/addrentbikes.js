57d2e170d3b490c0076ce1fed5c37eb5cd7eb77c
Your Name <bipingiri27@gmail.com>
2023-07-17 16:26:15 +0545

    file upload

--- adminSideModules/addrentbikes.js
+++ adminSideModules/addrentbikes.js
@@ -7,12 +7,23 @@ const Rentbike = require("../models/rentbikeSchema");
 const multer = require("multer");
 
 const upload = multer({ dest: "uploads/" });
-
+const cloudinary = require("cloudinary");
+// const ri = require('./src/routers')
+cloudinary.config({
+  cloud_name: "du2yndveo",
+  api_key: "336945855647363",
+  api_secret: "pTndbkaHkUvGe3Dpp6LkRSW66mM",
+});
 module.exports = router.post(
   "/addrentbikes",
   upload.single("myrentfile"),
   async (req, res, next) => {
     try {
+      let imageUrl;
+      if (req?.file) {
+        const url = await cloudinary.uploader.upload(req?.file?.path);
+        imageUrl = url?.secure_url;
+      }
       const data = new Rentbike({
         brand: req.body.brand,
         model: req.body.model,
@@ -21,14 +32,15 @@ module.exports = router.post(
         seats: req.body.seats,
         price: req.body.price,
         rent: req.body.rent,
-        fileName: req.file.originalname,
-        filePath: req.file.path,
+        fileName: req.file.filename,
+        filePath: imageUrl,
         fileType: req.file.mimetype,
         fileSize: req.file.size,
       });
       await data.save();
       res.status(201).send("Data uploaded successfully");
     } catch (error) {
+      throw error
       res.status(400).send(error.message);
     }
   }
